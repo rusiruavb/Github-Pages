c1b89b8aa4571008a1124dbde77f4fadfdaaffcd
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-02-06 14:06:33 +0530

    Change the app js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>
-          Hello World 45
+          Welcome to React
         </h3>
       </header>
     </div>
